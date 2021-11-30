cd8e31deb86ac8585ed81730a60cc4e4cb8d6167
Natasha Klaus <43129812+klausnat@users.noreply.github.com>
2021-11-30 09:04:35 +0300

    Update Exercises_04_From_Subset_SAT_Solver.v
    - Sublist added for list of vars. 
    - backtracking function added (we make map in process, trying vars one by one, splitting the situation for each variable from the list into two threads, where it is true and where it is false)

--- Exercises_04_From_Subset_SAT_Solver.v
+++ Exercises_04_From_Subset_SAT_Solver.v
@@ -6,86 +6,36 @@ Require Import List.
 Require Import Bool.Bool.
 
 Definition var := nat % type. 
-Definition total_map (A : Type) := var -> A.
-Definition t_empty {A : Type} (v : A) : total_map A :=
-  (fun _ => v).
-Definition t_update {A : Type} (m : total_map A)
-                    (x : var) (v : A) :=
+Definition total_map := var -> bool.
+Definition t_empty : total_map :=
+  (fun _ => false).
+Definition t_update (m : total_map)
+                    (x : var) (v : bool) :=
   fun x' => if PeanoNat.Nat.eqb x x' then v else m x'.
 (* to create an empty total map with default value *)
-Notation "'_' '!->' v" := (t_empty v)
+Notation "'_' '!->' v" := t_empty
                             (at level 100, right associativity).
 (* extending existing map with some bindings *)
 Notation "x '!->' v ';' m" := (t_update m x v)
                               (at level 100, v at next level, right associativity).
 
-Lemma t_update_eq : forall (A : Type) (m : total_map A) x v,
+Lemma t_update_eq : forall (m : total_map) x v,
   (x !-> v ; m) x = v.
-Proof. Admitted. 
+Proof. intros. 
+       unfold t_update. rewrite PeanoNat.Nat.eqb_refl. crush. Qed. 
 
-
-Definition partial_map (A : Type) := total_map (option A).
-Definition empty {A : Type} : partial_map A :=
-  t_empty None.
-Definition update {A : Type} (m : partial_map A)
-           (x : var) (v : A) :=
-  (x !-> Some v ; m).
- Notation "x '!->' v ';' m" := (update m x v)
-  (at level 100, v at next level, right associativity).
-(* We can also hide the last case when it is empty. *)
-Notation "x '!->' v" := (update empty x v)
-  (at level 100).
-
-Lemma t_apply_empty : forall (A : Type) (x : var) (v : A),
-  (_ !-> v) x = v.
-crush. Qed. 
-Lemma apply_empty : forall (A : Type) (x : var),
-  @empty A x = None.
-Proof. crush. Qed. 
-
-Lemma update_eq : forall (A : Type) (m : partial_map A) x v,
-  (x !-> v ; m) x = Some v.
-Proof.
-  intros. unfold update. rewrite t_update_eq.
-  reflexivity.
-Qed.
-
-Lemma update_shadow : forall (A : Type) (m : partial_map A) x v1 v2,
-  (x !-> v2 ; x !-> v1 ; m) = (x !-> v2 ; m). crush. Admitted. 
-
-Definition tvals := partial_map bool.
-
-Inductive literal : Set :=
-| Var : var -> literal
-| Not : var -> literal.               
+Definition tvals := total_map.
 
 Inductive formula : Set :=
-| Lit : literal -> formula.
-
-
-(*             (x \/ y \/ ~z) : Lit (Disj (Var x) (Disj (Var y) (Not z)))       *)
-
-Inductive formulaTrue : tvals -> formula -> Prop :=
-| TVar : forall tv var, tv var = Some true -> formulaTrue tv (Lit (Var var))
-| TNot : forall tv var, tv var = Some false -> formulaTrue tv (Lit (Not var)).
-
-Eval simpl in formulaTrue (fun _ => Some false) (Lit (Var 1)).
-
-Fixpoint vars_in_liter l : list var :=
-  match l with
-  | Var v => v :: nil
-  | Not v => v :: nil
-   end.                               
-
-Fixpoint vars_in_formula_dupl f : list var :=
-  match f with
-  | Lit l => vars_in_liter l
-   end.                                 
+| Var : var -> formula
+| Not : var -> formula
+| Disj : formula -> formula -> formula                        
+| Conj : formula -> formula -> formula.   
 
 Definition eq_nat_dec (n m : var) : {n = m} + {n <> m}.
 decide equality.
 Defined.
-
+(* 
 Definition In_lst (x : var) (ls : list var) : {In x ls} + {~(In x ls)}.
   induction ls.
   - crush.
@@ -104,18 +54,34 @@ Fixpoint remove_dups ls : list var :=
                  | right _ => x :: remove_dups xs 
                  end
   end.            
+ *)
 
-Fixpoint helper_makeAllMaps var (cur_maps : list tvals) : list tvals :=
-  match cur_maps with
-  | nil => nil
-  | map :: maps => (var !-> true ; map) :: helper_makeAllMaps var maps
-  end.                                                
+Definition nodup_var := nodup eq_nat_dec. 
 
-Fixpoint makeAllMaps (ls : list var) (cur_maps : list tvals) : list tvals :=
-            match ls return list tvals with
-            | nil => cur_maps 
-            | v :: vars => makeAllMaps vars ((helper_makeAllMaps v cur_maps) ++ cur_maps)
-            end.                  
+Fixpoint vars_in_formula f : list var :=
+  match f with
+  | Var v => v :: nil
+  | Not v => v :: nil
+  | Disj f1 f2 => nodup_var (vars_in_formula f1 ++ vars_in_formula f2)             
+  | Conj f1 f2 => nodup_var (vars_in_formula f1 ++ vars_in_formula f2)
+  end.                                 
+
+Inductive maybe (A : Set) (P : A -> Prop) : Set :=
+| Unknown : maybe P
+| Found : forall x : A, P x -> maybe P.
+
+(* we can define some new notations for convenient usage of type maybe. *)
+Notation "{{ x | P }}" := (maybe (fun x => P)).
+Notation "??" := (Unknown _ ).
+Notation "[| x |]" := (Found _ x _).
+
+Fixpoint evalFormula f map {struct f} : bool :=
+  match f with
+  | Var v => map v 
+  | Not v => negb (map v)  
+  | Disj f1 f2 => orb (evalFormula f1 map) (evalFormula f2 map)  
+  | Conj f1 f2 => andb (evalFormula f1 map) (evalFormula f2 map)
+  end.  
 
 Fixpoint CreateAllFalsesMap (ls : list var) map : tvals :=
   match ls with
@@ -123,9 +89,216 @@ Fixpoint CreateAllFalsesMap (ls : list var) map : tvals :=
   | x :: xs => CreateAllFalsesMap xs (x !-> false ; map) 
   end.                                 
 
-Eval simpl in CreateAllFalsesMap (1 :: 2 :: 3 :: nil) empty.
+Eval simpl in CreateAllFalsesMap (1 :: 2 :: 3 :: nil) t_empty.
+(* remove_dups (vars_in_formula_dupl f) - all vars in formula 
 
-Eval simpl in makeAllMaps (1 :: 2 :: 3 :: nil) ( (CreateAllFalsesMap (1 :: 2 :: 3 :: nil) empty) :: nil).
+checkVars начинает работу с карты, на которой all vars = false. 
+1) пробует изменить первую вару на тру. если нашли решение - возвращаем 
+2) 
+
+*)
+
+Definition checkVars : forall (ls : list var) (f : formula) (map : tvals), {{ map' | evalFormula f map' = true }}.
+  refine (fix F (ls : list var) (f : formula) (map : tvals) : {{ map' | evalFormula f map' = true }} := 
+  match ls with
+  | nil => _
+  | x :: xs => match F xs f (x !-> true; map) with
+               | ?? => F xs f (x !-> false; map)
+               | res => res
+               end
+  end).
+  destruct (evalFormula f map) eqn:E.
+  - eapply (Found _ map E).
+  - apply ??.
+Defined.     
+
+Definition f :=  (Disj (Var 1) (Disj (Var 2) (Not 3))).
+Eval simpl in checkVars (nodup_var (vars_in_formula f)) f t_empty.
+
+Definition f1 :=  (Conj (Var 1) (Not 1)).
+Eval simpl in checkVars ((vars_in_formula f1)) f1 t_empty.
+
+Search PeanoNat.Nat.eqb.
+Locate PeanoNat.Nat.eqb.
+Check PeanoNat.Nat.eqb.
+
+Print In. 
+(* отличается от библиотечного In тем, что возвращает bool, а не Prop *)
+Fixpoint In_var_ls' (x : var) (ls : list var) : bool :=
+  match ls with
+  | nil => false
+  | x' :: xs => match PeanoNat.Nat.eqb x x' with
+                | true => true
+                | false => In_var_ls' x xs
+                end                     
+  end.                                                                   
+
+Eval simpl in In_var_ls' 0 (1 :: 2 :: 3 :: nil).
+
+Locate reflect.
+Print Coq.Bool.Bool.reflect.
+(*
+Inductive reflect (P : Prop) : bool -> Set :=
+    ReflectT : P -> reflect P true | ReflectF : ~ P -> reflect P false
+*)
+Lemma In_reflect : forall x ls, reflect (In x ls) (In_var_ls' x ls).
+  intros. induction ls.
+  - crush.
+  - destruct (PeanoNat.Nat.eqb x a) eqn:E.
+    + crush. constructor. left.
+      apply PeanoNat.Nat.eqb_eq. rewrite PeanoNat.Nat.eqb_sym. auto.
+    + crush. inversion IHls. constructor. crush. constructor. crush. 
+      Search PeanoNat.Nat.eqb. assert (H' : PeanoNat.Nat.eqb x x = true ).
+      apply (PeanoNat.Nat.eqb_refl x). crush. 
+Qed.   
+
+(* Definition In_var_ls : forall x ls, {In_var_ls' x ls = true} + {~(In_var_ls' x ls = true)}. *)
+
+Inductive formula_List_Vars : list var -> formula -> Set := 
+| FVars_Var : forall v ls, In_var_ls' v ls  = true -> formula_List_Vars ls (Var v)
+| FVars_Not : forall v ls, In_var_ls' v ls = true -> formula_List_Vars ls (Not v)
+| FVars_Disj : forall f1 f2 ls1 ls2, formula_List_Vars ls1 f1->
+                                     formula_List_Vars ls2 f2 ->
+                                     formula_List_Vars (nodup_var (ls1 ++ ls2)) (Disj f1 f2)
+| FVars_Conj : forall f1 f2 ls1 ls2, formula_List_Vars ls1 f1 ->
+                                     formula_List_Vars ls2 f2 ->
+                                     formula_List_Vars (nodup_var (ls1 ++ ls2)) (Conj f1 f2).
+Search list. 
+Inductive formula_List_Vars_sub : list var -> formula -> Set := 
+| FVars_Var : forall v ls, In_var_ls' v ls  = true -> formula_List_Vars ls (Var v)
+| FVars_Not : forall v ls, In_var_ls' v ls = true -> formula_List_Vars ls (Not v)
+| FVars_Disj : forall f1 f2 ls1 ls2, formula_List_Vars ls1 f1->
+                                     formula_List_Vars ls2 f2 ->
+                                     formula_List_Vars (nodup_var (ls1 ++ ls2)) (Disj f1 f2)
+| FVars_Conj : forall f1 f2 ls1 ls2, formula_List_Vars ls1 f1 ->
+                                     formula_List_Vars ls2 f2 ->
+                                     formula_List_Vars (nodup_var (ls1 ++ ls2)) (Conj f1 f2).
+
+
+Lemma flv : forall f, formula_List_Vars (vars_in_formula f) f.
+  intros. induction f0.
+  - crush. constructor. crush. rewrite (PeanoNat.Nat.eqb_refl v). reflexivity.
+  - crush. constructor. crush. rewrite (PeanoNat.Nat.eqb_refl v). reflexivity.
+  - simpl. constructor. crush. crush.
+  - simpl. constructor. crush. crush. 
+Qed.
+
+Fixpoint eq_map (ls : list var) (m1 m2 : tvals) : bool :=
+  match ls with
+  | nil => true
+  | x :: xs => if eqb (m1 x) (m2 x) then eq_map xs m1 m2 else false
+  end.                                                              
+
+Notation "m1 '==(' vrs ')' m2" := (eq_map vrs m1 m2 = true) (at level 50).
+
+Lemma inFalse : forall v vrs m1, In_var_ls' v vrs = true -> (v !-> true ; m1) ==(vrs) (v !-> false ; m1). Admitted.
+
+Inductive formulaTrue : tvals -> formula -> Prop :=
+| TVar : forall tv var, tv var = true -> formulaTrue tv (Var var)
+| TNot : forall tv var, tv var = false -> formulaTrue tv (Not var)
+| TDisj : forall f1 f2 tv, formulaTrue tv f1 \/ formulaTrue tv f2 ->
+                           formulaTrue tv (Disj f1 f2)
+| TConj : forall f1 f2 tv, formulaTrue tv f1 -> formulaTrue tv f2 -> formulaTrue tv (Conj f1 f2). 
+
+Inductive formula_map : tvals -> formula -> Set :=
+| FM_Var_True : forall map var, map var = true -> formula_map map (Var var)
+| FM_Var_False : forall map var, map var = false -> formula_map map (Var var)
+| FM_Not_True : forall map var, map var = true -> formula_map map (Not var)
+| FM_Not_False : forall map var, map var = false -> formula_map map (Not var)
+| FM_Disj : forall f1 f2 map, formula_map map f1 ->
+                              formula_map map f2 ->
+                              formula_map map (Disj f1 f2)
+| FM_Conj : forall f1 f2 map, formula_map map f1 ->
+                              formula_map map f2 ->
+                              formula_map map (Conj f1 f2).
+
+Lemma t_empty_map : forall f, formula_map t_empty f.
+  intros. induction f0. apply FM_Var_False. unfold t_empty. auto.
+  apply FM_Not_False. unfold t_empty. auto. constructor. auto. crush. constructor. crush. crush. 
+Qed.
+
+Definition checkOneMap (f : formula) (map : tvals) : {formulaTrue map f} + {~formulaTrue map f}.
+  Hint Constructors formulaTrue.
+  induction f.
+  - destruct (map v) eqn:G. left. constructor. auto. right.
+      unfold not. intros. inversion H. crush. 
+  - destruct (map v) eqn:G. right; crush. inversion H. crush.
+    left. constructor. auto.
+  - inversion IHf1; inversion IHf2; crush. right. intros. inversion H1. crush.
+  - inversion IHf1; inversion IHf2; crush. right. intros. inversion H1. crush.
+    right. intros. inversion H1. crush. right. intros. inversion H1. crush. 
+Defined.   
+
+Lemma not_nil : forall f, formula_List_Vars nil f -> forall truth, ~ formulaTrue truth f.
+  intros.
+  induction f0.
+  - crush. inversion H. inversion H3.
+  - crush. inversion H. inversion H3.
+  - unfold not. intros. inversion H. crush.
+    inversion H0.
+    (* доказать, что ls1 = nil /\ ls2 = nil, subst.  *)
+Admitted. 
+
+Definition CheckFormulaHelp : forall f ls map, formula_List_Vars_sub ls f -> formula_map map f -> {truth : tvals | formulaTrue truth f } + {forall truth, ~ formulaTrue truth f }.
+refine (fix F f ls map pf1 pf2 : {truth : tvals | formulaTrue truth f } + {forall truth, ~ formulaTrue truth f } :=
+          match ls return {truth : tvals | formulaTrue truth f } + {forall truth, ~ formulaTrue truth f } with
+          | nil => _
+          | x :: xs => match F f xs (x !-> true; map) _ _ with
+                       | ?? => F xs f (x !-> false; map)
+                       | res => res
+                       end
+          end).                  
+- admit. 
+- 
+
+  Definition CheckFormula (f : formula) : {truth : tvals | formulaTrue truth f } + {forall truth, ~ formulaTrue truth f } := @CheckFormulaHelp f (vars_in_formula f) t_empty (flv f) (t_empty_map f). 
+
+
+Definition checkFormula : forall (f : formula) (pf :  formula_List_Vars (vars_in_formula f) f),
+    {truth : tvals | evalFormula f truth = true } + {forall truth, evalFormula f truth = false}.
+refine (fix F f pf : {truth : tvals | evalFormula f truth = true } + {forall truth, evalFormula f truth = false} :=
+          match f return {truth : tvals | evalFormula f truth = true } + {forall truth, evalFormula f truth = false} with
+          | Var v => _
+          | Not v => _
+          | Disj f1 f2 => _
+          | Conj f1 f2 => _
+          end).                  
+
+(* Definition CheckFormulaHelp : forall f ls map, formula_List_Vars ls f -> formula_map map f -> *)
+(*   {truth : tvals | formula_map truth f -> formulaDenote truth f = true} + {forall truth, formula_map truth f ->  evalFormula truth f = false}. *)
+
+Definition checkVars : forall (ls : list var) (f : formula) (map : tvals), {{ map' | evalFormula f map' = true }}.
+
+1. как показать, что если ls = nil то map уже не удастся изменять. Не нашли
+2. как показать, что если map изменить нельзя, то не существует возможной оценки.
+3. что значит, что map изменять нельзя.
+
+Definition checkVars' : forall (ls : list var) (f : formula) (map : tvals), { map' | evalFormula f map' = true } + {
+forall map', map'
+evalFormula f map' = false
+                                                                                                                   }.
+  
+  refine (fix F (ls : list var) (f : formula) (map : tvals) : { map' | evalFormula f map' = true } + {forall map', evalFormula f map' = false} := 
+  match ls with
+  | nil => _
+  | x :: xs => match F xs f (x !-> true; map) with
+               | ?? => F xs f (x !-> false; map)
+               | res => res
+               end
+  end).
+  destruct (evalFormula f map) eqn:E.
+  - eapply (Found _ map E).
+  - apply ??.
+Defined.     
+
+(* BELOW NAT"S DEFINITIONS *)
+
+
+
+
+Inductive formulaTrue : tvals -> formula -> Prop :=
+| TVar : forall tv var, tv var = Some true -> formulaTrue tv (Lit (Var var))
+| TNot : forall tv var, tv var = Some false -> formulaTrue tv (Lit (Not var)).
 
 (* что список принадлежит формуле (это ее контекст) with duplicates, I probably don't need this Prop *)
 Inductive formula_List_Vars : list var -> formula -> Prop := 
@@ -165,6 +338,12 @@ Theorem FAM : forall f t, formula_all_maps (t :: nil) f -> ~ formulaTrue t f -> 
   intros. unfold not. intros.
 inversion X. crush. crush. Qed. 
 
+(* ПЛАН РАБОТЫ 
+0) ввести длину списка в formula_all_maps
+0) переписать определение (In map tvals) чтобы оно было ориентировано на tvals 
+1) создать подтип от formula_all_maps, formula_all_maps_sub : list tvals -> formula -> n
+*)
+
 Definition checkFormula : forall f : formula, {truth : tvals | formulaTrue truth f } + {forall truth, ~formulaTrue truth f }.
   intros f.
   assert (H : formula_List_Vars (remove_dups (vars_in_formula_dupl f)) f ).
@@ -180,22 +359,16 @@ Definition checkFormula : forall f : formula, {truth : tvals | formulaTrue truth
   remember (remove_dups (vars_in_formula_dupl f)) as fav.
   remember (makeAllMaps fav (CreateAllFalsesMap fav empty :: nil)) as fam.
   clear Heqfav H Heqfam fav.
-    
-  generalize dependent f. generalize dependent fam.
+    generalize dependent f. generalize dependent fam.
   refine (fix F fam f pf :  {truth : tvals | formulaTrue truth f} + {forall truth : tvals, ~ formulaTrue truth f} :=
             match f return  {truth : tvals | formulaTrue truth f} + {forall truth : tvals, ~ formulaTrue truth f}
             with
-            | Lit (Var v) => match fam as ls with
-                             | nil => _
-                             | x :: xs => _
-                             end               
-            | Lit (Not v) => match fam as ls with
-                             | nil => _
-                             | x :: xs => _
-                             end               
+            | Lit (Var v) => _
+            | Lit (Not v) => _
             end).                 
-      
-  apply all_maps_not_nil in G. inversion G.
+  - destruct fam.
+    + crush. apply all_maps_not_nil in pf. inversion pf.
+    + 
 
 
   
@@ -204,16 +377,3 @@ Definition checkFormula : forall f : formula, {truth : tvals | formulaTrue truth
   - left. exists x. auto. 
   - 
     
-  (* -----------  ПЛАН - не пробовала, не знаю как реализовать детали ----------- 
-0. до запуска рефайна, даем формуле мапу, на которой все переменные false. 
-1. ищем не присвоенные vars в формуле
-2. если найдена - разбиваем формулу на 2 поддерева : 
-         - в первом этой вар присваиваем true, 
-         - во втором - false
-   вызываем рекурсивно на обоих поддеревьях пункт 1.  
-3. если не найдена - проверяем истинна ли формула. 
-
-
-
-
-*)
